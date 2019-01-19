98abe62e4ecaa3cfd9c5fa86c1b60170d1f41299
Jacob Stoebel <jstoebel@gmail.com>
2019-01-19 06:49:47 -0500

    all user to create a todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,21 @@
 class TodosController < ApplicationController
   def index
+    @todos = Todo.all
   end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    Todo.create todos_params
+    redirect_to todos_path
+  end
+
+  private
+
+  def todos_params
+    params.require(:todo).permit(:title)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'todos#index'
+  resources :todos, only: %i[index new create]
 end
 
